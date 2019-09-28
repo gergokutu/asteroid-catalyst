8c851e28144f699a033528b2e05866ac6db5c6a9
gergokutu <gergo.kutu@gmail.com>
2019-09-28 21:41:14 +0200

    GetData algorithm gives back asteroid timestamps

--- asteroids_lv1.js
+++ asteroids_lv1.js
@@ -1,43 +1,39 @@
 const fs = require('fs')
 
 const getData = (callback) => {
-  fs.readFile('lvl1-0.inp.html', function(err, contents) {
+  fs.readFile('lvl1-4.inp.html', function(err, contents) {
     return callback(contents.toString())
   })
 }
 
 getData(response => {
-  console.log('RAW:', response)
-
+  // create array from imported data by every new line
   const rawInput = response.split('\n')
+
+  // shift method changes the original rawInput
   const firstLineOfRawInput = rawInput.shift()
   const firstLineInArray = firstLineOfRawInput.split(' ')
-  console.log('firstLineInArray:', firstLineInArray)
 
   const myInputLength = rawInput.length - 1
   const data = rawInput.splice(0, myInputLength).map(array => array.split(' '))
-  console.log('asteroid info:', data)
-
   const numOfRowsInABlock = parseInt(data[0][1], 10) + 1
-  console.log('numOfRowsInABlock:', numOfRowsInABlock)
-
   const numOfBlocks = data.length / numOfRowsInABlock
-  console.log('numOfBlocks:', numOfBlocks)
 
   const timeStamps = []
 
-  const block = data.splice(0, numOfRowsInABlock)
-  const timeStampRow = block.shift()
-  const sumOfPicRows = block.map(row => row.reduce((accumulator, currentValue) => parseInt(accumulator + currentValue)))
-  const blockSum = sumOfPicRows.reduce((accumulator, currentValue) => accumulator + currentValue)
-  console.log('timeStampRow:', timeStampRow)
-  console.log('block:', block)
-  console.log('sumOfPicRows:', sumOfPicRows)
-  console.log('blockSum:', blockSum)
-  
-  if (blockSum !== 0) {
-    timeStamps.push(parseInt(timeStampRow[0]))
-    console.log('timeStamp:', timeStamps)
+  // following part should be repeated » numOfBlocks times
+  // every iteration should check the next block...
+
+  for (let index = 0; index < numOfBlocks * numOfRowsInABlock; index = index + numOfRowsInABlock) {
+    const block = data.splice(0, numOfRowsInABlock)
+    const timeStampRow = block.shift()
+    const sumOfPicRows = block.map(row => row.reduce((accumulator, currentValue) => parseInt(accumulator + currentValue)))
+    const blockSum = sumOfPicRows.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+    if (blockSum !== 0) {
+      timeStamps.push(parseInt(timeStampRow[0]))
+      console.log('timeStamp:', timeStamps)
+    }
   }
 })
 
